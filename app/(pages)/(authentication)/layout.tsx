5740faecf7d735f4d1c4b178603c4767c252f107
Leonard-Prosper <leonardmalcolm00@gmail.com>
2023-12-12 22:48:33 +0100

    Completed Sign in page (HTML)

--- app/(pages)/(authentication)/layout.tsx
+++ app/(pages)/(authentication)/layout.tsx
@@ -3,17 +3,17 @@ import Image from "next/image";
 const AuthenticationLayout = ({ children }: { children: React.ReactNode }) => {
     return (  
         <div className="w-full h-[100svh] pb-16 bg-dark-400 overflow-y-auto">
-            <div className="w-[350px] mx-auto">
+            <div className="w-full sm:w-[350px] px-6 mx-auto">
                 <Image 
                     src="/crossbase-logo-white.svg" 
                     alt="crossbase.ai" 
                     width={0} 
                     height={0} 
-                    className="h-auto w-auto mx-auto my-10" 
+                    className="h-auto w-auto sm:mx-auto my-10" 
                 />
                 {children}
             </div>
-            <footer className="absolute bottom-0 bg-dark-400 w-full py-5 border-t-2 border-dark-200 text-xs text-light-400 text-center">
+            <footer className="max-sm:hidden absolute bottom-0 bg-dark-400 w-full py-5 border-t-2 border-dark-200 text-xs text-light-400 text-center">
                 © 2023 Crossbase Inc.  All Rights Reserved
             </footer>
         </div>
