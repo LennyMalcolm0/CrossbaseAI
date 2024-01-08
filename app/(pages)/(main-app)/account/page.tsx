a19c072290fa926351aba485c2f14c00c2cc600c
Leonard-Prosper <leonardmalcolm00@gmail.com>
2024-01-08 02:49:18 +0100

    Completed Account Page UI update

--- app/(pages)/(main-app)/account/page.tsx
+++ app/(pages)/(main-app)/account/page.tsx
@@ -7,73 +7,65 @@ const Account = () => {
     const [update, setUpdate] = useState(false);
     
     return (  
-        <main className="h-full app-container">
-            <section className="w-full rounded-[20px] border-2 border-dark-200 relative mb-5">
-                <div className="flex items-center justify-between p-5 text-white border-b border-dark-200">
+        <main className="h-full app-container py-5 max-sm:flex max-sm:flex-col justify-between">
+            <section className="w-full rounded-[20px] bg-light-400 border-2 border-light-200 relative mb-5">
+                <div className="flex items-center justify-between font-bold p-5 border-b border-light-200">
                     <h1>Profile</h1>
                     {update ? (
                         <button 
                             onClick={() => setUpdate(prev => !prev)}
-                            className="flex items-center gap-2 text-xs text-primary-200"
+                            className="flex items-center gap-1.5 text-xs text-primary-400"
                         >
                             <span>Cancel</span>
-                            <CgCloseO className="text-xl leading-[1] hover:text-light-200 cursor-pointer" />
+                            <CgCloseO className="text-[18px] leading-[1] cursor-pointer" />
                         </button>
                     ):(
                         <button 
                             onClick={() => setUpdate(prev => !prev)}
-                            className="flex items-center gap-2 text-xs text-primary-100"
+                            className="flex items-center gap-1.5 text-xs text-primary-400"
                         >
                             <span>Edit</span>
-                            <CiEdit className="text-xl leading-[1] hover:text-light-200 cursor-pointer" />
+                            <CiEdit className="text-[18px] leading-[1] cursor-pointer" />
                         </button>
                     )}
                 </div>
                 <div className="p-5 grid grid-cols-1 sm:grid-cols-2 gap-x-5 gap-y-[15px] text-xs">
-                    <div className="sm:col-span-2 flex gap-5">
+                    <div className="sm:col-span-2 flex gap-5 max-sm:gap-3">
                         <div className="w-full">
-                            <label htmlFor="firstName" className="text-light-400 font-medium capitalize">First Name</label>
+                            <label htmlFor="firstName" className="text-dark-400 leading-[1.6] font-medium capitalize">First Name</label>
                             <input 
                                 type="text"
                                 name="firstName"
                                 disabled={!update}
-                                className={`w-full py-[9px] px-3.5 rounded-[40px] bg-transparent border border-dark-100 text-white 
-                                    placeholder:text-light-400 shadow-[0px_1px_2px_0px_rgba(16,24,40,0.05)] mt-2
-                                `}
+                                className="profile-input"
                             />
                         </div>
                         <div className="w-full">
-                            <label htmlFor="lastName" className="text-light-400 font-medium capitalize">Last Name</label>
+                            <label htmlFor="lastName" className="text-dark-400 leading-[1.6] font-medium capitalize">Last Name</label>
                             <input 
                                 type="text"
                                 name="lastName"
                                 disabled={!update}
-                                className={`w-full py-[9px] px-3.5 rounded-[40px] bg-transparent border border-dark-100 text-white 
-                                    placeholder:text-light-400 shadow-[0px_1px_2px_0px_rgba(16,24,40,0.05)] mt-2
-                                `}
+                                className="profile-input"
                             />
                         </div>
                     </div>
                     <div className="w-full">
-                        <label htmlFor="emailAddress" className="text-light-400 font-medium capitalize">Email Address</label>
+                        <label htmlFor="emailAddress" className="text-dark-400 leading-[1.6] font-medium capitalize">Email Address</label>
                         <input 
                             type="text"
                             name="emailAddress"
                             disabled={!update}
-                            className={`w-full py-[9px] px-3.5 rounded-[40px] bg-transparent border border-dark-100 text-white 
-                                placeholder:text-light-400 shadow-[0px_1px_2px_0px_rgba(16,24,40,0.05)] mt-2
-                            `}
+                            className="profile-input"
                         />
                     </div>
                     <div className="w-full">
-                        <label htmlFor="phoneNumber" className="text-light-400 font-medium capitalize">Phone Number</label>
+                        <label htmlFor="phoneNumber" className="text-dark-400 leading-[1.6] font-medium capitalize">Phone Number</label>
                         <input 
                             type="text"
                             name="phoneNumber"
                             disabled={!update}
-                            className={`w-full py-[9px] px-3.5 rounded-[40px] bg-transparent border border-dark-100 text-white 
-                                placeholder:text-light-400 shadow-[0px_1px_2px_0px_rgba(16,24,40,0.05)] mt-2
-                            `}
+                            className="profile-input"
                         />
                     </div>
                 </div>
@@ -81,13 +73,20 @@ const Account = () => {
                     <div className="px-5 pb-5 flex justify-between">
                         <div></div>
                         <button 
-                            className="ml-auto py-3 px-6 bg-primary-100 text-sm 
-                            text-dark-400 font-bold hover:bg-light-200"
+                            className="max-sm:w-full ml-auto py-[15px] px-10 text-sm text-primary-400 font-bold max-sm:mt-3
+                            hover:bg-primary-400 hover:text-light-400 shadow-[0px_1px_2px_0px_rgba(16,24,40,0.05)]
+                            border-2 border-primary-400 max-sm:bg-primary-400 max-sm:text-light-400"
                         > Update
                         </button>
                     </div>
                 )}
             </section>
+            <button 
+                className="max-sm:w-full ml-auto py-[15px] px-10 bg-light-400 text-sm text-primary-400 font-bold 
+                hover:bg-primary-400 hover:text-light-400 shadow-[0px_1px_2px_0px_rgba(16,24,40,0.05)]
+                border-2 border-primary-400 max-sm:bg-primary-400 max-sm:text-light-400"
+            > Logout
+            </button>
         </main>
     );
 }
