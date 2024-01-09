03746aeaf9eb1646d3208c49e646620251ea7987
Leonard-Prosper <leonardmalcolm00@gmail.com>
2024-01-10 00:51:00 +0100

    Added logic to stream prompt response

--- app/(pages)/(main-app)/page.tsx
+++ app/(pages)/(main-app)/page.tsx
@@ -5,39 +5,63 @@ import { PiPaperPlaneRightFill } from "react-icons/pi";
 import { TbCloudDownload } from "react-icons/tb";
 import { RiPushpin2Line } from "react-icons/ri";
 import { FaPlus } from "react-icons/fa6";
-import { useRef } from "react";
-import { useClickAway } from "ahooks";
+import { useRef, useState } from "react";
+import { getCurrentUser } from "@/app/utils/auth";
+import { HttpClient } from "@/app/utils/axiosRequests";
 
 const Home = () => {
     const textareaRef = useRef<HTMLTextAreaElement>(null);
-    const textareaContainerRef = useRef<HTMLDivElement>(null);
+    const [textareaValue, setTextareaValue] = useState("");
+    const [response, setResponse] = useState("");
  
     const handleInput = () => {
         const textarea = textareaRef.current;
-        const parentDiv = textareaContainerRef.current;
-        if (!textarea || !parentDiv) return;
+        if (!textarea) return;
 
-        if (textarea.value.trim() === "") {
-            parentDiv.style.height = "100px";
-            return
-        }
- 
-        // Adjust the height of the parent div according to the textarea's content
-        if (textarea.scrollHeight > textarea.offsetHeight) {
-            parentDiv.style.height = `${Math.min(textarea.scrollHeight + 50, 400)}px`;
-        }
+        textarea.style.height = 'auto';
+
+        const minimumHeight = Math.max(textarea.scrollHeight, 76);
+        const newHeight = Math.min(minimumHeight, 300);
+
+        textarea.style.height = `${newHeight}px`;
     };
 
-    useClickAway(() => {
-        const textarea = textareaRef.current;
-        const parentDiv = textareaContainerRef.current;
-        if (!textarea || !parentDiv) return;
+    const handlePrompt = async (e: any) => {
+        e.preventDefault();
 
-        if (textarea.value.trim() === "") {
-            parentDiv.style.height = "100px";
-            return
+        const prompt = textareaValue;
+        setTextareaValue("");
+
+        const user = await getCurrentUser();
+        if (!user || !prompt) return;
+
+        const userIdToken = await user.getIdToken();
+
+        const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_API_URL}/stream`, {
+            method: "POST",
+            body: JSON.stringify({ prompt }),
+            headers: {
+                Accept: "text/event-stream",
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${userIdToken}`
+            }
+        });
+        
+        if (response.ok && response.body) {
+            const reader = response.body
+                .pipeThrough(new TextDecoderStream())
+                .getReader();
+        
+            while (true) {
+                const { value, done } = await reader.read();
+                if (done) break;
+                console.log("Received: ", value);
+                setResponse((prev) => prev + value);
+            }
+        } else {
+            console.error("Stream response was not ok.");
         }
-    }, [textareaContainerRef]);
+    }
     
     return (   
         <main className="h-full lg:py-5">
@@ -54,8 +78,8 @@ const Home = () => {
                         </div>
                     </div>
                 </section>
-                <section className="grow lg:border-2 lg:rounded-[20px] lg:border-light-200 relative overflow-hidden">
-                    <div className="sm:hidden absolute top-0 h-[110px] w-full primary-gradient" />
+                <section className="conversation-box lg:border-2 lg:rounded-[20px] lg:border-light-200 relative overflow-hidden">
+                    <div className="sm:hid den absolute top-0 h-[110px] w-full primary-gradient" />
                     <div className="w-full h-full flex flex-col justify-between relative z-[9999]">
                         <div className="max-sm:px-5 lg:px-7 py-4 text-[18px] font-bold text-dark-200 flex items-center justify-between">
                             <h1 className="flex items-center gap-1.5">
@@ -105,19 +129,40 @@ const Home = () => {
                                     </div>
                                 ))}
                             </div>
+                            {/* <div 
+                                className="py-[13px] px-[15px] w-full rounded-[10px] bg-light-400 flex 
+                                items-center gap-[15px] cursor-pointer hover:shadow-sm"
+                                dangerouslySetInnerHTML={{ __html: response }}
+                            ></div> */}
+                            <div className="w-full flex flex-col mt-[80px]">
+                                <p className="user w-fit max-w-[95%] pt-2.5 pb-[15px] px-3.5 rounded-b-[8px] mb-5">
+                                In this example, we set the Content-Type header to and include the data you want to send in the body. 
+                                </p>
+                                <p className="assistant w-fit max-w-[95%] pt-2.5 pb-[15px] px-3.5 rounded-b-[8px] mb-5">
+                                Please note that if {"you're"} trying to implement server-sent events, the Content-Type is used for the server response, not for the client request.
+                                </p>
+                            </div>
                         </section>
-                        <form className="sticky bottom-0 max-sm:px-5 lg:px-7 py-5 w-full bg-light-300 text-sm text-dark-300">
-                            <div 
-                                ref={textareaContainerRef} 
-                                className="h-[100px] w-full px-3.5 py-3 bg-light-400 rounded-[15px] border 
-                                border-primary-100 shadow-[0px_0px_5px_4px_rgba(246,208,145,0.20)] flex gap-2.5"
+                        <form 
+                            onSubmit={handlePrompt}
+                            className="sticky bottom-0 max-sm:px-5 lg:px-7 py-5 w-full bg-light-300 text-sm text-dark-300"
+                        >
+                            <div className="w-full px-3.5 py-3 bg-light-400 rounded-[15px] border flex gap-2.5 
+                                border-primary-100 shadow-[0px_0px_5px_4px_rgba(246,208,145,0.20)]"
                             >
                                 <textarea 
                                     ref={textareaRef}
                                     name="prompt" 
                                     placeholder="Ask me anything about your store..."
-                                    className="prompt w-full h-auto bg-transparent focus:outline-none resize-none"
+                                    value={textareaValue}
+                                    onChange={(e) => setTextareaValue(e.target.value)}
+                                    className="prompt w-full h-[76px] bg-transparent focus:outline-none resize-none"
                                     onInput={handleInput}
+                                    onKeyDown={(e) => {
+                                        if (e.key === 'Enter') {
+                                            handlePrompt(e);
+                                        }
+                                    }}
                                 />
                                 <button type="submit" className="text-2xl text-primary-400 mt-auto">
                                     <PiPaperPlaneRightFill />
